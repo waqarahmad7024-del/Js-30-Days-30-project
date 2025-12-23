722645d1a4f25b3092f0e98db805cff87f90d91a
waqarahmad7024-del <waqarahmad7024@gmail.com>
2025-12-23 13:02:07 +0500

    Arrow Function

---
+++ Arrow Function/script.js
@@ -0,0 +1,13 @@
+
+let hello = () => {
+   return "Hello Waqar"
+}
+
+console.log(hello());
+
+
+let welcome = (name, age) => {
+    return `Hello and Welcome name ${name} - age ${age}`
+}
+
+console.log(welcome("waqar", 45));
